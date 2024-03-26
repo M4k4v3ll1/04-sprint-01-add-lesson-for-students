2e0d68ba30a47952f562a690cd756410fed05df8
Ivan Trandin <trandinii@gmail.com>
2024-03-26 18:38:24 +0400

    deckAPI wrote, added axios.get for decks

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,15 @@
 import s from './DecksList.module.css'
+import {AppRootState, useAppDispatch} from "../../../app/store";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getDecksTC} from "../decks-reducer";
 
 export const DecksList = () => {
-  return <ul className={s.list}></ul>
+    const dispatch = useAppDispatch()
+    const decks = useSelector<AppRootState, any[]>(state => state.decks.decks)
+    useEffect(() => {
+        dispatch(getDecksTC())
+        //deckAPI.fetchDecks()
+    }, [])
+    return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -1,17 +1,31 @@
+import {deckAPI} from "./decks-api";
+import {Dispatch} from "redux";
+
 const initialState = {
-  decks: [] as any[], // todo: add type
-  searchParams: {
-    name: '',
-  },
+    decks: [] as any[], // todo: add type
+    searchParams: {
+        name: '',
+    },
 }
 
 type DecksState = typeof initialState
 
 export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
-  switch (action.type) {
-    default:
-      return state
-  }
+    switch (action.type) {
+        case "GET-DECKS":
+            return {...state, decks: action.decks}
+        default:
+            return state
+    }
 }
 
-type DecksActions = any
+export const getDecksAC = (decks: any[]) => ({type: 'GET-DECKS', decks} as const)
+
+export const getDecksTC = () => (dispatch: Dispatch<DecksActions>) => {
+    deckAPI.fetchDecks()
+        .then(res => {
+            dispatch(getDecksAC(res.data))
+        })
+}
+
+type DecksActions = ReturnType<typeof getDecksAC>
